c86982720db438e6c4ecc6d293a14c1f51480da3
Daniel Dew <daniel.dew007@gmail.com>
2026-05-02 17:59:31 +0100

    Tests pass. to test CRC macro conflict

--- STM32/STM32-F4Nucleo-HAL-Tests/Core/Src/main.cpp
+++ STM32/STM32-F4Nucleo-HAL-Tests/Core/Src/main.cpp
@@ -58,6 +58,7 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 #include "smart-serial/port/mock_port.hpp"
+#include "smart-serial/crc.hpp"
 Smart_serial::Mock_port<256, 256> port;
 Smart_serial::Clock::STM32_hal_clock hal_clk;
 Smart_serial::Slave slave(port, hal_clk, 0x02U, 0xFE, 0xAAU, 1000U);
@@ -80,19 +81,25 @@ int main(void)
 	HAL_Init();
 
   /* USER CODE BEGIN Init */
-
-	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
-	hal_clk.delay(1000U);
-	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-	hal_clk.delay(1000U);
-	// test slave
+	Smart_serial::Frame::Frame handshakeFrame {
+		Smart_serial::Frame::Frame_header {
+			0xAAU,
+			0xFEU,
+			0x02U,
+			0x06,
+			0U,
+		},
+		{}
+	};
+	Smart_serial::Frame::Raw_frame rFrame;
+	Smart_serial::Frame::dump_frame(&rFrame, &handshakeFrame);
+	uint16_t crc = Smart_serial::CRC_::compute_crc16(&rFrame);
+	Smart_serial::CRC_::append_crc16(&rFrame, 256U, rFrame.length, crc);
 	slave.set_auto_handshake(true);
-	slave.send_response("Hello World", 0x12U);
-	const uint8_t* s = reinterpret_cast<const uint8_t*>("Hello World");
-	uint16_t t = sizeof(s);
+	port.inject_rx(rFrame.data, rFrame.length, false);
+	Smart_serial::Frame::Frame reqFrame;
+	slave.receive_request(&reqFrame, 1000U);
 	const uint8_t* txBuf = port.get_tx_data();
-	uint16_t txLen = port.get_tx_data_len();
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
