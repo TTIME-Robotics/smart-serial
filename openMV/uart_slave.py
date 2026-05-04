e17488570f6f07be5f2ffc9139855def9378b54f
Daniel Dew <daniel.dew007@gmail.com>
2026-05-04 22:57:17 +0100

    im genuinely the best (TESTS ✅ )
    tested (works)
    Handshake works
    Send string works (although not always, noise..?)

--- openMV/uart_slave.py
+++ openMV/uart_slave.py
@@ -20,7 +20,7 @@ class uart_slave:
     STOP_PROCESSING_CMD = 0xD2
     START_PROCESSING_CMD = 0xD1
 
-    def crc16_ccitt(data: bytes, poly: int = 0x1021, init: int = 0xFFFF) -> int:
+    def crc16_ccitt(self, data, poly: int = 0x1021, init: int = 0xFFFF) -> int:
         crc = init
         for byte in data:
             crc ^= byte << 8
@@ -44,19 +44,25 @@ class uart_slave:
             byte1 = self.uart_port.read(1)
         if self.uart_port.any() > 0:
             byte2 = self.uart_port.read(1)
-        foundCrc = struct.unpack("<h", f"{byte1}{byte2}")
+        foundCrc = struct.unpack("<H", byte1 + byte2)[0]
         return self.crc16_ccitt(data) == foundCrc
 
-
+    ACK_BYTE = 0x06
+    NACK_BYTE = 0x15
     def getIsCmd(self, cmd_byte) -> bool:
         if self.uart_port.any() < 7:
             return False
-        data = self.uart_port.read(7)
+        data = self.uart_port.read(5)
         if data[0] == self.START_BYTE:
             if data[1] == self.THIS_ADRESS and data[2] == self.MASTER_ADRESS:
                 isRightCmd = (data[3] == cmd_byte)
-                correctCrc = self.verifyCRC16([data[5], data[6]])
-                return (correctCrc and isRightCmd)
+                correctCrc = self.verifyCRC16(data)
+                if correctCrc:
+                    if isRightCmd:
+                        return True
+                    elif data[3] == self.ACK_BYTE and data[4] == 0 and self.autoHandshake:
+                        self.sendPayload("", self.ACK_BYTE)
+        return False
 
     def getSendData(self):
         return self.getIsCmd(self.SEND_DATA_CMD)
@@ -65,19 +71,29 @@ class uart_slave:
     def getStopProcessing(self):
         return self.getIsCmd(self.STOP_PROCESSING_CMD)
 
-    def sendPayload(self, str_, cmd_byte):
-        dataToSend = [
+    autoHandshake = True
+    def setAutoHandshake(self, shouldShake):
+        self.autoHandshake = shouldShake
+
+    def sendPayload(self, payload, cmd_byte):
+        # Accept either str or bytes for payload
+        if isinstance(payload, str):
+            payload = payload.encode()  # str -> bytes
+        print(payload)
+
+        dataToSend = bytearray([
             self.START_BYTE,
             self.MASTER_ADRESS,
             self.THIS_ADRESS,
             cmd_byte,
-            len(str_)
-        ]
-        dataToSend.append(str_)
-        crc = self.crc16_ccitt(dataToSend).to_bytes(2, 'little')
-        dataToSend.append(crc)
-        self.uart_port.write(dataToSend)
+            len(payload),
+        ])
+        dataToSend.extend(payload)
 
+        crc = self.crc16_ccitt(dataToSend)
+        dataToSend.extend(crc.to_bytes(2, 'little'))
+
+        self.uart_port.write(dataToSend)
 
 slave_device = uart_slave()
 
@@ -87,16 +103,18 @@ sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
 sensor.skip_frames(time=2000)  # Wait for settings take effect.
 clock = time.clock()  # Create a clock object to track the FPS.
 
+keepChecking = True
 while True:
     clock.tick()  # Update the FPS clock.
     img = sensor.snapshot()  # Take a picture and return the image.
-    keepChecking = True
     if keepChecking:
         if slave_device.getStartProcessing():
+            slave_device.sendPayload("Hello World!", 0x06)
             print ("DONE")
-            keepChecking = False
+            keepChecking = True
         else:
-            print(clock.fps())
+            time.sleep(1)
+            print("None")
 
     # Note: OpenMV Cam runs about half as fast when connected
     # to the IDE. The FPS should increase once disconnected.
